102dda979d8a02c6604d636a6636167294152cb2
azharhanif <83354175+azharhanif@users.noreply.github.com>
2026-04-16 11:22:48 -0400

    Update comments for project submission guidelines
    Added comments regarding project submission and grading rubric.

--- FinalProjects1_5/Proj4(SolutionProvided)/team_solution.py
+++ FinalProjects1_5/Proj4(SolutionProvided)/team_solution.py
@@ -1,2 +1,12 @@
 # Team members will build their project here.
 # Team finally submit this file as the final solution with proper comments
+# Presentation coding challenge
+# Total 100: Grading rubric will follow as instructed in the project description.
+
+### 50% on the submitted code
+
+### 50% on the Presentation coding challenge 
+
+# **What is Presentation coding challenge?**
+
+# During your scheduled project presentation there will be an in class code understanding  and verification challenge specific to your final project for individual team members.
