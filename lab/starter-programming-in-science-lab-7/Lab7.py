74c21617108088d18c56137356e40b29a6fc2833
azharhanif <83354175+azharhanif@users.noreply.github.com>
2026-08-21 15:46:29 -0400

    Delete lab/starter-programming-in-science-lab-7 directory

--- lab/starter-programming-in-science-lab-7/Lab7.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Function 1: Rotate the Array
-# This function creates an array of a specified size, fills it with numbers starting from `starting_integer` and increasing by 3,
-# then rotates elements at even indices 2 positions to the left.
-def rotate_the_array(array_size, starting_integer):
-   return []
-
